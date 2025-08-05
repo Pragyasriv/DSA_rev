259eef9b84f2403ceb01173c25053fa3b838e8bb
Pragya Srivastava <105738394+Pragyasriv@users.noreply.github.com>
2025-08-05 15:27:15 +0530

    Sort a stack using a temporary stack
    Given a stack of integers, sort it in ascending order using another temporary stack.

---
+++ Stacks/sortAStack.java
@@ -0,0 +1,34 @@
+import java.util.Stack;
+
+public class sortAStack {
+    public static Stack<Integer> sortStack(Stack<Integer> input) {
+        Stack<Integer> tempStack = new Stack<>();
+
+        while(!input.isEmpty()) {
+            int temp = input.pop();
+            while(!tempStack.isEmpty() && tempStack.peek() <= temp) {
+                input.push(tempStack.pop());
+            }
+            tempStack.push(temp);
+        }
+        return tempStack;
+    }
+
+    public static void main(String args[])
+    {
+        Stack<Integer> input = new Stack<Integer>();
+        input.add(34);
+        input.add(3);
+        input.add(31);
+        input.add(98);
+        input.add(92);
+        input.add(23);
+    
+        // This is the temporary stack
+        Stack<Integer> tmpStack=sortStack(input);
+        while (!tmpStack.empty())
+        {
+            System.out.print(tmpStack.pop()+" ");
+        } 
+    }
+}
